007063036a0b23a85ea5026205ad4e1766789992
golamrosulislam74-dotcom <golamrosulislam74@gmail.com>
2026-03-22 13:54:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,14 @@
 
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Blog is Back!</h1>
+      <p>Start rebuilding your components here.</p>
+    </div>
+  );
+}
+
+export default App;
 
