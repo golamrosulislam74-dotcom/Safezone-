2da3c9f22c4e8343ecdab9bd1dc684d9d6ea130c
golamrosulislam74-dotcom <golamrosulislam74@gmail.com>
2026-03-22 13:51:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,1 @@
 
-// This script runs automatically on your Blog
-console.log("GitHub Engine v2.0 Connected!");
-
-// 1. Logic Change: Show a welcome message to confirm the link
-alert("GitHub Update Detected! Switching to Neon Mode.");
-
-// 2. Feature Change: Add a glow effect when you hover the preview
-const previewBox = document.querySelector('.preview-stage');
-if(previewBox) {
-    previewBox.onmouseenter = () => {
-        previewBox.style.boxShadow = "0 0 50px #a855f7";
-    };
-    previewBox.onmouseleave = () => {
-        previewBox.style.boxShadow = "none";
-    };
-}
