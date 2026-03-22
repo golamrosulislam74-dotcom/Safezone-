c53591c436504ece1b73a71915cc43393cb4f22c
golamrosulislam74-dotcom <golamrosulislam74@gmail.com>
2026-03-22 13:56:05 +0530

    Remove App component and its content

--- App.js
+++ App.js
@@ -1,14 +1,1 @@
 
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>My Blog is Back!</h1>
-      <p>Start rebuilding your components here.</p>
-    </div>
-  );
-}
-
-export default App;
-
